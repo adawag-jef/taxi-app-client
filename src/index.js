9769ca63b47775fa891f1f763398f2ca2e95c2f3
jeff <jeff.salarium@gmail.com>
2022-06-21 14:55:15 +0800

    feat: authentication

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import "bootswatch/dist/lumen/bootstrap.css";
 import { HashRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
+import axios from "axios";
+
+axios.defaults.xsrfCookieName = "csrftoken";
+axios.defaults.xsrfHeaderName = "X-CSRFToken";
 
 ReactDOM.render(
   // changed
